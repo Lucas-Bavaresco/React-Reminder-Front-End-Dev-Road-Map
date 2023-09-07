afebf9405a062ba25c99e828a9707c053f79570e
lucas-bavaresco <l.bavaresco.pereira@gmail.com>
2023-09-06 22:11:05 -0300

    v001

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import reminderLogo from "../../assets/images/Logo.jpg";
+import { Button } from "../Button";
+import { Container } from "./styles";
+import { NewStickyModal } from "../newStickyModal";
+
+export function Header() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function handleOpenModal() {
+    setIsModalOpen(true);
+  }
+
+  function handleCloseModal() {
+    setIsModalOpen(false);
+  }
+
+  return (
+    <Container>
+      <img src={reminderLogo} alt="Logo React Reminder" />
+
+      <Button title="Adicionar Lembrete" onClick={handleOpenModal} />
+
+      <NewStickyModal isOpen={isModalOpen} onRequestClose={handleCloseModal} />
+    </Container>
+  );
+}
